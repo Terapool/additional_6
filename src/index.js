bd53e72827454478e381bafae210db86aa8f3c97
Sergey Dmitriev <terapool@gmail.com>
2017-09-29 11:38:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
 module.exports = function zeros(expression) {
-  var exprParse = expression.split('*');
-var multiArray =[];
-var str, number;
-var countTens = 0, countOdds =0, countFives= 0;
-var result;
-for (var i=0; i <exprParse.length; i++ ) {
-    str = exprParse[i];
-    if (str[str.length-1] ==='!' && (str[str.length-2] ==='!')) { //!! case
-        number = parseInt(str.substring(0,str.length - 2));
-        do {
-            multiArray.push(number);
-            number = number -2;
-        } while (number >0);
-    } else { // ! case
-        number = parseInt(str.substring(0,str.length - 1));
-        do {
-            multiArray.push(number);
-            number = number - 1;
-        } while (number >0);
+
+    var exprParse = expression.split('*');
+    var multiArray =[];
+    var str, number;
+    var countTwo =0, countFive= 0;
+    for (var i=0; i <exprParse.length; i++ ) {
+        str = exprParse[i];
+        if (str[str.length-1] ==='!' && (str[str.length-2] ==='!')) { //!! case
+            number = parseInt(str.substring(0,str.length - 2));
+            do {
+                multiArray.push(number);
+                number = number -2;
+            } while (number >0);
+        } else { // ! case
+            number = parseInt(str.substring(0,str.length - 1));
+            do {
+                multiArray.push(number);
+                number = number - 1;
+            } while (number >0);
+        }
     }
-}
-for (var i = 0; i < multiArray.length; i++ ) {
-    if (multiArray[i]%10 == 0) {
-        countTens++;
+    for (var i = 0; i < multiArray.length; i++ ) {
+
+        if (multiArray[i]%5 == 0) {
+            number = multiArray[i];
+            do {
+                countFive++;
+                number = number/5;
+            } while (number%5 == 0);
+        }
+        if (multiArray[i]%2 == 0) {
+            number = multiArray[i];
+            do {
+                countTwo++;
+                number = number/2;
+            } while (number%2 == 0);
+        }
     }
-    if (multiArray[i]%2==0 && multiArray[i]%10 != 0) {
-        countOdds++;
-    }
-    if (multiArray[i]%5==0 && multiArray[i]%10 != 0) {
-        countFives++;
-    }
-      if (multiArray[i]%50==0) {
-        countFives++;
-    }
-}
-result = countTens;
-if (countOdds < countFives) {result = result + countOdds} else {result = result + countFives};
-return result;
+    return countTwo < countFive ? countTwo : countFive;
 }
